39d85f54ebed940b620e574216ebb63be091809e
Rody Huancas <rodyhuancas.04@gmail.com>
2023-01-18 20:35:58 -0500

    js completo

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,22 +3,46 @@ const submit = document.querySelector(".submit");
 submit.addEventListener("click", (e) => {
     // prevenir el evento por defecto
     e.preventDefault();
-    const inputs = document.querySelectorAll(".input");
-    const formGroup = document.querySelectorAll(".field");
-    const errores = document.querySelectorAll(".error");
 
-    inputs.forEach(input => {
-        formGroup.forEach(form => {
-            errores.forEach(error => {
-                if (input.value === "") {
-                    form.style.border = "2px solid red";
-                    error.classList.remove("active")
-                } else {
-                    form.style.border = "2px solid hsl(246, 25%, 77%)";
-                    error.classList.add("active")
-                }
-            })
+    // field
+    const field = document.querySelectorAll(".field");
+
+    // name
+    const inputName = document.querySelector("#name");
+    const errorName = document.querySelectorAll(".error-name");
+    // last name
+    const inputLast = document.querySelector("#lastName");
+    const errorLast = document.querySelectorAll(".error-last");
+    // email
+    const inputEmail = document.querySelector("#email");
+    const errorEmail = document.querySelectorAll(".error-email");
+    // password
+    const inputPassword = document.querySelector("#password");
+    const errorPassword = document.querySelectorAll(".error-password");
+
+    // Validaciones
+    validation(inputName.value, field, errorName);
+    validation(inputLast.value, field, errorLast);
+    validation(inputEmail.value, field, errorEmail);
+    validation(inputPassword.value, field, errorPassword);
+
+})
+
+const validation = (value, fields, errors) => {
+    errors.forEach(error => {
+        fields.forEach(field => {
+            if (value === "") {
+                field.style.borderColor = "red";
+                error.classList.remove("active");
+                // desaparecer mensaje de error después de 3 segundos
+                setTimeout(() => {
+                    field.style.borderColor = "hsl(246, 25%, 77%)";
+                    error.classList.add("active");
+                }, 3000);
+            } else {
+                error.classList.add("active");
+                field.style.borderColor = "hsl(246, 25%, 77%)";
+            }
         })
-    })
-
-});
+    });
+}
